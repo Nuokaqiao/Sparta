f04e098821c3e09d1c71a9a70c72c0fc002b3bc4
石基宏 <shijihong@inke.cn>
2019-12-08 21:52:54 +0800

    fix path name

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	_ "myproject/routers"
 	"github.com/astaxie/beego"
+	_ "sparta/routers"
 )
 
 func main() {
 	beego.Run()
 }
-
--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,10 @@
 package routers
 
 import (
-	"myproject/controllers"
 	"github.com/astaxie/beego"
+	"sparta/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 }
